0b556ed7cdbd636a03b0f4ce69cdd9fc713c81b8
Egor Efymenko <efymenko@internet.ru>
2025-01-26 15:29:21 +0300

    configure the LSP with Mason

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 require("settings/basic_options")
 require("lazy/lazy")
+
+require("mason").setup()
+require("mason-lspconfig").setup {
+    ensure_installed = { "lua_ls", "clangd" },
+}
+
+require("mason-lspconfig").setup_handlers {
+    function (server_name)
+        require("lspconfig")[server_name].setup {}
+    end,
+}
