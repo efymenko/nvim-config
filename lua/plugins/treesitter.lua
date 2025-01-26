a828efb941bdf083a556951c336320bc6c904b96
Egor Efymenko <efymenko@internet.ru>
2025-01-26 15:45:39 +0300

    add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        opts = function()
+            return require "settings/treesitter"
+        end,
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+        end,
+    }
+}
