69853f9d01bb46dd97c5f9915855822971826502
Egor Efymenko <efymenko@internet.ru>
2025-01-26 16:40:17 +0300

    add a lspsaga

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,5 +2,16 @@ return {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
-    "linrongbin16/lsp-progress.nvim"
+    {
+        "linrongbin16/lsp-progress.nvim",
+        opts = {},
+    },
+    {
+        'nvimdev/lspsaga.nvim',
+        opts = {},
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+            'nvim-tree/nvim-web-devicons',
+        }
+    }
 }
